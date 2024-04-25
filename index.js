8258013e27c59ad59a38b7754bf78bbcc990b8df
https://github.com/rinanizza <rinanizza77@gmail.com>
2024-04-25 20:21:50 +0100

    header-perser-microservice

--- index.js
+++ index.js
@@ -20,11 +20,21 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+// Endpoint to return user information
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress: ipAddress, language: language, software: software });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
+
+
